303eec147b7896c145225ce5710c131bf7b98559
Abigael <abiagel.desandi@gmail.com>
2021-01-20 13:22:07 +0100

    finalisation du carrousel

---
+++ public/js/js.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    $('#slideshow .slider').slick({
+      prevArrow: '<img class="priv_arrow" src="./public/img/prevArrow.png" alt="">',
+      nextArrow: '<img class="next_arrow" src="./public/img/nextArrow.png" alt="">',
+    });
+  });
