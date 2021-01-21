4b98f8c49068d26a302f0e301f7c2fab6913afd2
Abigael <abiagel.desandi@gmail.com>
2021-01-21 15:25:32 +0100

    ajout menu stické

--- public/js/js.js
+++ public/js/js.js
@@ -1,6 +1,31 @@
 $(document).ready(() => {
-    $('#slideshow .slider').slick({
-      prevArrow: '<img class="priv_arrow" src="./public/img/prevArrow.png" alt="">',
-      nextArrow: '<img class="next_arrow" src="./public/img/nextArrow.png" alt="">',
-    });
+  $('#slideshow .slider').slick({
+    prevArrow: '<img class="priv_arrow" src="./public/img/prevArrow.png" alt="">',
+    nextArrow: '<img class="next_arrow" src="./public/img/nextArrow.png" alt="">',
   });
+});
+
+/*
+var positionElementInPage = $('.nav-stick').offset().top;
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= positionElementInPage) {
+  // fixed
+  $('.nav-stick').addClass("floatable");
+  } else {
+  // relative
+  $('.nav-stick').removeClass("floatable");
+  }
+});*/
+
+$(".nav-stick").hide();
+
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 600 ) {
+        $('.nav-stick').fadeIn(200);
+    } else {
+        $('.nav-stick').fadeOut(200);
+    }
+  })
+});   
+
